2acc82a7103cbf0fdd6a35bfe24caea936fad90e
Morgan Skillicorn <hello@morgan.games>
2020-11-02 04:02:40 +0000

    added state switcher and pathfinding

---
+++ Game Mechanics/Assets/Ragdoll/EnemyController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+	[SerializeField]
+	public NavMeshAgent agent;
+	
+	[SerializeField]
+	public GameObject destin;
+	
+    // Update is called once per frame
+    void Update()
+	{
+		agent.SetDestination(destin.transform.position);
+    }
+}
